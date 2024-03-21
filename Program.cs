b7b584b1f089a32852be6933795c399774d67c4b
aufa19 <abdillahaufa79@gmail.com>
2024-03-21 10:10:46 +0700

    menambahkan calss Penjumlahan dengan method JumlahTigaAngka

--- Program.cs
+++ Program.cs
@@ -2,6 +2,19 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        penjumlahan jumlah = new penjumlahan();
+        jumlah.jumlahTigaAngka(13,02,22);
+    }
+}
+
+public class penjumlahan
+{
+    public void jumlahTigaAngka<T>(T angka1, T angka2, T angka3)
+    {
+        dynamic dy1 = angka1;
+        dynamic dy2 = angka2;
+        dynamic dy3 = angka3;
+        dynamic jumlah = dy1+ dy2+ dy3;
+        Console.WriteLine(jumlah);
     }
 }
